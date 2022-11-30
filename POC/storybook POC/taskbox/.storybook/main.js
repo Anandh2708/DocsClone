ac3415f9183300862ce29f4b7ae2338338a21eac
Goms <gomathinayagam.a@brillio.com>
2022-11-30 16:33:44 +0530

    storybook

---
+++ POC/storybook POC/taskbox/.storybook/main.js
@@ -0,0 +1,18 @@
+module.exports = {
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    '@storybook/addon-a11y',
+  ],
+  framework: "@storybook/angular",
+  core: {
+    builder: "@storybook/builder-webpack5",
+  },
+  features: {
+    interactionsDebugger: true,
+  },
+};
+
+
