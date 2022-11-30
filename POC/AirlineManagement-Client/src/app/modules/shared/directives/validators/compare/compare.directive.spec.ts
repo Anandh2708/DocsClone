365978b41fd4aaaaafe8a17ba40673026d6711f8
Anandh <anandh27081999@gamil.com>
2022-11-30 16:27:54 +0530

    delete

--- POC/AirlineManagement-Client/src/app/modules/shared/directives/validators/compare/compare.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CompareDirective } from './compare.directive';
-
-describe('CompareDirective', () => {
-  it('should create an instance', () => {
-    const directive = new CompareDirective();
-    expect(directive).toBeTruthy();
-  });
-});
